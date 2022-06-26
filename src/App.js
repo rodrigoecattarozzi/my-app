2ec40940d6c04d9e184c24470134e3a8da45a998
Rodrigo Cattarozzi <rodrigoecattarozzi@gmail.com>
2022-06-26 13:07:48 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,29 +1,21 @@
 import React from 'react';
 import NavBar from './NavBar';
 import FooterCmp from './FooterCmp';
+import SearchModule from './SearchModule';
 
-import logo from './logo.svg';
 import './App.css';
 import './materialize.css';
 
 function App() {
+  
+
   return (
     <div className="App">
       <NavBar />
 
       <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Today
-        </a>
+        <SearchModule />
+        
       </header>
 
       <FooterCmp />
